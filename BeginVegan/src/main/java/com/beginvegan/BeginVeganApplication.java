aaf4529f43e4b3954081355a5fc50da18ff209aa
adorableyou26 <adorableyou26@gmail.com>
2023-05-11 18:57:38 +0900

    2023-05-11 성찬민 first commit

---
+++ BeginVegan/src/main/java/com/beginvegan/BeginVeganApplication.java
@@ -0,0 +1,13 @@
+package com.beginvegan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginVeganApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeginVeganApplication.class, args);
+    }
+
+}
