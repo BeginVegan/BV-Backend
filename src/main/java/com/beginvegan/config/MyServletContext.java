bae890a3ff323eabc9c53188c534d03cd8266628
adorableyou26 <adorableyou26@gmail.com>
2023-05-19 13:17:00 +0900

    2023-05-19 성찬민 CORS 코드 추가

--- src/main/java/com/beginvegan/config/MyServletContext.java
+++ src/main/java/com/beginvegan/config/MyServletContext.java
@@ -1,9 +1,15 @@
 package com.beginvegan.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class MyServletContext {
+public class MyServletContext implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true).allowedOrigins("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 }
