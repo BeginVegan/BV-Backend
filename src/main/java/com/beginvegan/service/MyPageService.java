aa1f21006244a22adf70daa26a67dc6ad4b7fa0c
cmoon <chihun0507@naver.com>
2023-05-26 13:27:23 +0900

    feat: controller service repository 치훈 할당분 완성 초안

--- src/main/java/com/beginvegan/service/MyPageService.java
+++ src/main/java/com/beginvegan/service/MyPageService.java
@@ -1,7 +1,11 @@
 package com.beginvegan.service;
 
+import com.beginvegan.dto.BookmarkDTO;
 import com.beginvegan.dto.ReviewDTO;
+import com.beginvegan.exception.AddException;
 import com.beginvegan.exception.FindException;
+import com.beginvegan.exception.ModifyException;
+import com.beginvegan.exception.RemoveException;
 import com.beginvegan.repository.MemberRepository;
 import com.beginvegan.repository.ReviewRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -23,4 +27,31 @@ public class MyPageService {
         return reviewRepository.selectAllReviewByMemberEmail(userEmail);
     }
 
+    public List<ReviewDTO> findAllReviewByRestaurantId(int id) throws FindException {
+        return reviewRepository.selectAllReviewByRestaurantId(id);
+    }
+
+    public void addReview(ReviewDTO reviewInfo) throws AddException {
+        reviewRepository.insertReview(reviewInfo);
+    }
+
+    public void removeReview(int reviewNo) throws RemoveException {
+        reviewRepository.deleteReview(reviewNo);
+    }
+
+    public void modifyReview(int reviewNo, ReviewDTO reviewInfo) throws ModifyException {
+        reviewRepository.updateReview(reviewNo, reviewInfo);
+    }
+
+    public void addBookmark(String userEmail, int restaurantNo) throws AddException {
+        memberRepository.insertBookmark(restaurantNo, userEmail);
+    }
+
+    public void removeBookmark(String userEmail, int restaurantNo) throws RemoveException {
+        memberRepository.deleteBookmark(restaurantNo, userEmail);
+    }
+
+    public List<BookmarkDTO> findAllBookmarkByMemberEmail(String memberEmail) throws FindException {
+        return memberRepository.selectAllBookmarkByMemberEmail(memberEmail);
+    }
 }
