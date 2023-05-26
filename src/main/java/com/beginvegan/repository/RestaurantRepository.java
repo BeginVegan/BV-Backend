fa2fa53625d005807788295c480e439b46b8b70a
Dayeon Park <ddggs@naver.com>
2023-05-26 10:32:16 +0900

    feat: RestaurantMapper, RestaurantRepository 구현

--- src/main/java/com/beginvegan/repository/RestaurantRepository.java
+++ src/main/java/com/beginvegan/repository/RestaurantRepository.java
@@ -1,7 +1,10 @@
 package com.beginvegan.repository;
 
+import com.beginvegan.dto.MenuDTO;
 import com.beginvegan.dto.RestaurantDTO;
+import com.beginvegan.exception.AddException;
 import com.beginvegan.exception.FindException;
+import com.beginvegan.exception.RemoveException;
 
 import java.util.List;
 
@@ -12,4 +15,35 @@ public interface RestaurantRepository {
      * @throws FindException Restaurant 테이블에 데이터가 없을 때 발생하는 Exception
      */
     public List<RestaurantDTO> selectAllRestaurant() throws FindException;
+
+    /**
+     * restaurantNo와 일치하는 식당의 정보를 반환한다.
+     * @param restaurantNo 조회할 식당 번호
+     * @return 식당 정보
+     * @throws FindException 식당 정보 조회에 실패할 경우 발생
+     */
+    public RestaurantDTO selectRestaurantByRestaurantNo(int restaurantNo) throws FindException;
+
+    /**
+     * DB에 새로운 식당을 추가한다.
+     * @param restaurantInfo 추가할 식당 정보
+     * @return insert한 식당의 restaurant_no
+     * @throws AddException 데이터 추가에 실패할 경우 발생
+     */
+    public int insertRestaurant(RestaurantDTO restaurantInfo) throws AddException;
+
+    /**
+     * DB에 새로운 메뉴를 추가한다.
+     * @param restaurantNo 메뉴들이 속한 식당의 번호
+     * @param menuList 추가할 메뉴들
+     * @throws AddException 데이터 추가에 실패할 경우 발생
+     */
+    public void insertRestaurantMenu(int restaurantNo, List<MenuDTO> menuList) throws AddException;
+
+    /**
+     * 식당 하나를 삭제한다.
+     * @param restaurantNo 삭제할 식당 번호
+     * @throws RemoveException 데이터 삭제에 실패할 경우 발생
+     */
+    public void deleteRestaurant(int restaurantNo) throws RemoveException;
 }
--- src/main/java/com/beginvegan/repository/RestaurantRepositoryMariaDB.java
+++ src/main/java/com/beginvegan/repository/RestaurantRepositoryMariaDB.java
@@ -1,14 +1,19 @@
 package com.beginvegan.repository;
 
+import com.beginvegan.dto.MenuDTO;
 import com.beginvegan.dto.RestaurantDTO;
+import com.beginvegan.exception.AddException;
 import com.beginvegan.exception.FindException;
+import com.beginvegan.exception.RemoveException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Repository("RestaurantRepository")
@@ -26,12 +31,86 @@ public class RestaurantRepositoryMariaDB implements RestaurantRepository{
             List<RestaurantDTO> restaurantList = session.selectList("com.beginvegan.mybatis.RestaurantMapper.selectAllRestaurant");
             return restaurantList;
         } catch (Exception e) {
-            e.printStackTrace(); //나중에 하기
+            e.printStackTrace();
+            throw new FindException(e.getMessage());
         } finally {
             if (session != null) {
                 session.close();
             }
         }
-        return null;
+    }
+
+    @Override
+    public RestaurantDTO selectRestaurantByRestaurantNo(int restaurantNo) throws FindException {
+        SqlSession session = null;
+
+        try {
+            session = sqlSessionFactory.openSession();
+            RestaurantDTO restaurantInfo = session.selectOne("com.beginvegan.mybatis.RestaurantMapper.selectRestaurantByRestaurantNo");
+            return restaurantInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new FindException(e.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public int insertRestaurant(RestaurantDTO restaurantInfo) throws AddException {
+        SqlSession session = null;
+
+        try {
+            session = sqlSessionFactory.openSession();
+            session.insert("com.beginvegan.mybatis.RestaurantMapper.insertRestaurant", restaurantInfo);
+            return restaurantInfo.getRestaurantNo();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new AddException(e.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void insertRestaurantMenu(int restaurantNo, List<MenuDTO> menuList) throws AddException {
+        SqlSession session = null;
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("restaurantNo", restaurantNo);
+        map.put("menuList", menuList);
+
+        try {
+            session = sqlSessionFactory.openSession();
+            session.insert("com.beginvegan.mybatis.RestaurantMapper.insertRestaurantMenu", map);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new AddException(e.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void deleteRestaurant(int restaurantNo) throws RemoveException {
+        SqlSession session = null;
+
+        try {
+            session = sqlSessionFactory.openSession();
+            session.delete("com.beginvegan.mybatis.RestaurantMapper.deleteRestaurant", restaurantNo);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RemoveException(e.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
