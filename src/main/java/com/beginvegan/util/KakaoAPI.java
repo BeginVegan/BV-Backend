5a5633d31349d5188cacb1f494a08c359a8645b8
adorableyou26 <adorableyou26@gmail.com>
2023-05-30 12:17:34 +0900

    feat: MemberServiceTEST 코드 작성
    MemberServiceTEST 코드 작성
    - 로그인 API 테스트 포함

--- src/main/java/com/beginvegan/util/KakaoAPI.java
+++ src/main/java/com/beginvegan/util/KakaoAPI.java
@@ -3,39 +3,41 @@ package com.beginvegan.util;
 import com.beginvegan.dto.MemberDTO;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.*;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.Map;
 
 public class KakaoAPI {
-    public static final String USER_EMAIL_URL = "https://kapi.kakao.com/v2/user/me";
+    private static final String KAKAO_API = "https://kapi.kakao.com/v2/user/me";
 
-    public static MemberDTO getMemberInfo(String accessToken) throws Exception {
-        RestTemplate restTemplate = new RestTemplate();
+    public static MemberDTO getMemberInfo(String accessToken) throws IOException {
+        WebClient client = WebClient.builder().baseUrl(KAKAO_API).build();
 
-        //요청 헤더를 세팅한다.
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.setBearerAuth(accessToken);
+        ResponseEntity<String> response = client.get()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .retrieve()
+                .toEntity(String.class)
+                .block();
 
-        //위에서 세팅한 파라미터와 헤더를 바탕으로 요청 entity를 만들고, GET 요청을 보낸다.
-        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(USER_EMAIL_URL, HttpMethod.GET, entity,String.class);
+        if (response == null || response.getBody() == null) {
+            throw new IOException("Kakao API 호출 실패");
+        }
 
-        //응답의 JSON 문자열을 Parsing한다. List의 첫번째 요소가 email값으로 온다.
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = mapper.readValue(response.getBody(), new TypeReference<Map<String, Object>>() {});
-        Map<String, Object> accountInfo = (Map<String, Object>) map.get("kakao_account");
+        Map<String, Object> kakaoResponse = mapper.readValue(response.getBody(), new TypeReference<Map<String, Object>>() {});
+        String id = String.valueOf(kakaoResponse.get("id"));
+        String name = (String)((Map<String, Object>) kakaoResponse.get("properties")).get("nickname");
+        String email = id.concat("@kakao.com");
 
-        //Map에서 name, email을 가져온다.
         MemberDTO memberInfo = new MemberDTO();
-        memberInfo.setMemberEmail((String) accountInfo.get("email"));
-        memberInfo.setMemberName((String) accountInfo.get("nickname"));
+        memberInfo.setMemberName(name);
+        memberInfo.setMemberEmail(email);
 
         return memberInfo;
     }
-
 }
