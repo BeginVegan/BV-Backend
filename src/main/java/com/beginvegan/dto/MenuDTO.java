de6301fedd68b9675068532731facfcf1da6ecbe
hasunghwa <tjdghk3920@naver.com>
2023-06-01 12:00:54 +0900

    feat: ReservationMenuDTO 부모 생성자 호출하는 생성자 구현

--- src/main/java/com/beginvegan/dto/MenuDTO.java
+++ src/main/java/com/beginvegan/dto/MenuDTO.java
@@ -1,8 +1,12 @@
 package com.beginvegan.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MenuDTO {
     private int menuNo; // menu_no
     private int restaurantNo; // restaurant_no
