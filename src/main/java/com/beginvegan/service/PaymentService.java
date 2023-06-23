d3ef3dccc3c6763a673bb63ed986547726101163
hasunghwa <tjdghk3920@naver.com>
2023-06-23 18:21:19 +0900

    feat: 결제 취소 시 포인트 적립되도록

--- src/main/java/com/beginvegan/service/PaymentService.java
+++ src/main/java/com/beginvegan/service/PaymentService.java
@@ -1,12 +1,17 @@
 package com.beginvegan.service;
 
+import com.beginvegan.dto.MemberDTO;
 import com.beginvegan.dto.PaymentDTO;
+import com.beginvegan.dto.PointDTO;
+import com.beginvegan.dto.ReservationDTO;
 import com.beginvegan.exception.AddException;
 import com.beginvegan.exception.FindException;
 import com.beginvegan.exception.ModifyException;
 import com.beginvegan.exception.RemoveException;
+import com.beginvegan.repository.MemberRepository;
 import com.beginvegan.repository.PaymentRepository;
 import com.beginvegan.repository.ReservationRepository;
+import com.beginvegan.util.TimeUtil;
 import com.siot.IamportRestClient.request.CancelData;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
@@ -14,14 +19,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 @Service("paymentService")
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
-    public PaymentService(PaymentRepository paymentRepository) {
+    private final ReservationRepository reservationRepository;
+    private final MemberRepository memberRepository;
+
+    public PaymentService(PaymentRepository paymentRepository, ReservationRepository reservationRepository, MemberRepository memberRepository) {
         this.paymentRepository = paymentRepository;
+        this.reservationRepository = reservationRepository;
+        this.memberRepository = memberRepository;
     }
 
     public PaymentDTO findPaymentByImpUid(String impUid) throws FindException {
@@ -48,7 +60,29 @@ public class PaymentService {
         return paymentRepository.updatePayment(paymentDTO);
     }
 
-    public String deletePayment(String impUid) throws RemoveException {
+    private void changePoint(ReservationDTO reservationDTO, String pointDiv, boolean isPlus) throws FindException, ModifyException, ParseException, AddException {
+        MemberDTO memberDTO = memberRepository.selectMemberByMemberEmail(reservationDTO.getMemberEmail());
+        int pointResult = memberDTO.getMemberPoint() + (isPlus ? +reservationDTO.getReservationDiscount() : -reservationDTO.getReservationDiscount());
+
+        memberDTO.setMemberPoint(pointResult);
+        memberRepository.updateMemberPoint(memberDTO);
+
+        PointDTO pointDTO = PointDTO.builder()
+                .memberEmail(memberDTO.getMemberEmail())
+                .pointDiv(pointDiv)
+                .pointTime(TimeUtil.toDateTime(new Date()))
+                .pointChange(reservationDTO.getReservationDiscount())
+                .pointResult(pointResult)
+                .build();
+
+        memberRepository.insertPoint(pointDTO);
+    }
+
+    @Transactional
+    public String deletePayment(String impUid) throws RemoveException, FindException, ModifyException, ParseException, AddException {
+        int reservationNo = paymentRepository.selectPaymentByImpUid(impUid).getReservationNo();
+        ReservationDTO reservationInfo = reservationRepository.selectReservationByReservationNo(reservationNo);
+        changePoint(reservationInfo, "Purchase Cancel", true);
         return paymentRepository.deletePayment(impUid);
     }
 }
